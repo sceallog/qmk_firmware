4d5b706334e2354b3dcb3c0e27a81390a18d890f
Sven Buettner <sven.buettner1@gmail.com>
2023-01-30 22:59:16 +0900

    Add files for planck

---
+++ keyboards/planck/ez/glow/keymaps/sceallog/rules.mk
@@ -0,0 +1,13 @@
+SRC += muse.c
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+AUTO_SHIFT_ENABLE = yes
+STENO_ENABLE = yes
+STENO_PROTOCOL = geminipr
+KEYBOARD_SHARED_EP = yes
+MOUSEKEY_ENABLE = no
+TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
